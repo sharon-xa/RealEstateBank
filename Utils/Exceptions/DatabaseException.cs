9b74db05998d933d12e32330dd9ccd5dab4316e7
sharon <ali93456@gmail.com>
2025-08-13 02:34:03 +0300

    refactor: update DatabaseException class to include repository and method properties

--- Utils/Exceptions/DatabaseException.cs
+++ Utils/Exceptions/DatabaseException.cs
@@ -2,14 +2,20 @@ namespace RealEstateBank.Utils.Exceptions;
 
 public class DatabaseException : Exception {
     public int StatusCode { get; }
-    public string ErrorCode { get; }
+    public string? SqlState { get; }
+    public string Repository { get; set; }
+    public string Method { get; set; }
 
     public DatabaseException(
         string message,
+        string repository,
+        string method,
         int statusCode = StatusCodes.Status500InternalServerError,
-        string errorCode = "DATABASE_ERROR"
+        string sqlState = "DATABASE_ERROR"
     ) : base(message) {
         StatusCode = statusCode;
-        ErrorCode = errorCode;
+        SqlState = sqlState;
+        Repository = repository;
+        Method = method;
     }
 }
